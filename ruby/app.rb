abc29a2ad43e6ddb658c7ce3fcd243fab330fdd5
tompng <tomoyapenguin@gmail.com>
2017-10-21 19:08:26 +0900

    history n1fix

--- ruby/app.rb
+++ ruby/app.rb
@@ -241,18 +241,19 @@ class App < Sinatra::Base
     statement = db.prepare('SELECT * FROM message WHERE channel_id = ? ORDER BY id DESC LIMIT ? OFFSET ?')
     rows = statement.execute(@channel_id, n, (@page - 1) * n).to_a
     statement.close
-    @messages = []
-    rows.each do |row|
-      r = {}
-      r['id'] = row['id']
-      statement = db.prepare('SELECT name, display_name, avatar_icon FROM user WHERE id = ?')
-      r['user'] = statement.execute(row['user_id']).first
-      r['date'] = row['created_at'].strftime("%Y/%m/%d %H:%M:%S")
-      r['content'] = row['content']
-      @messages << r
-      statement.close
-    end
-    @messages.reverse!
+    @messages = rows.map do |row|
+      user = User.find(row['user_id'.freeze])
+      {
+        id: row['id'.freeze],
+        user: {
+          name: user['name'.freeze],
+          display_name: user['display_name'.freeze],
+          avatar_icon: user['avatar_icon'.freeze]
+        },
+        date: row['created_at'].strftime("%Y/%m/%d %H:%M:%S"),
+        content: row['content']
+      }
+    end.reverse
 
     statement = db.prepare('SELECT COUNT(*) as cnt FROM message WHERE channel_id = ?')
     cnt = statement.execute(@channel_id).first['cnt'].to_f
